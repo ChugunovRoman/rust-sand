21ce470a6999569d8840fdcf80c8230209bbc1d2
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-11-13 09:55:07 +0300

    Implement 1.1 task

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,67 @@
+use std::fmt;
+
+#[derive(Copy, Clone, Default)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Clone)]
+struct Polyline {
+    points: Option<Vec<Point>>,
+}
+
+impl Polyline {
+    pub fn new() -> Self {
+        Polyline { points: None }
+    }
+
+    pub fn from_vec(points: Vec<Point>) -> Self {
+        Polyline {
+            points: Some(points),
+        }
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Point (x: {}, y: {})", self.x, self.y)
+    }
+}
+
+impl fmt::Display for Polyline {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut s: Vec<String> = vec!["Polyline (".to_owned()];
+
+        if let Some(pts) = &self.points {
+            for point in pts {
+                s.push(format!("[{};{}]", point.x, point.y));
+            }
+        }
+
+        s.push(")".to_owned());
+
+        write!(f, "{}", s.join(" "))
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    let point1 = Point::default();
+    let point2 = Point {
+        x: 40,
+        ..Point::default()
+    };
+    let copy_point2 = point2;
+
+    println!("Point 1 with default values: {}", point1);
+    println!("Point 2 with y default value: {}", point1);
+    println!("Point 3 is copy of Point 2: {}", copy_point2);
+
+    let points1 = Polyline::new();
+    let points2 = Polyline::from_vec(vec![point1, point2, copy_point2]);
+    let points3 = points2.clone();
+
+    println!("{}", points1);
+    println!("{}", points2);
+    println!("Clone of points 2: {}", points3);
 }
