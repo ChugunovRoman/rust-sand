c439d3a88d7b2f122d63c5802f86c41588a4685c
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-11-13 21:00:58 +0300

    Start to implement 1_2 task

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -1,3 +1,101 @@
+use std::fmt::{self, Debug, Display};
+use std::pin::Pin;
+use std::rc::Rc;
+
+trait MutMeSomehow {
+    fn mut_me_somehow(self: Pin<&mut Self>);
+}
+
+trait SayHi {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from");
+    }
+}
+
+impl<i32> SayHi for Box<i32>
+where
+    i32: Debug,
+{
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from Box: {:?}", self)
+    }
+}
+
+impl<i32> MutMeSomehow for Box<&mut i32> {
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        let s = **self;
+        *s = 0;
+//        let p = unsafe { self.get_unchecked_mut() }.as_mut();
+//        *p = 0;
+    }
+}
+
+// impl<T> SayHi for Rc<T> {
+//     fn say_hi(self: Pin<&Self>) {
+//         println!("Hi from Rc")
+//     }
+// }
+
+// impl<T> MutMeSomehow for Rc<T> {
+//     fn mut_me_somehow(self: Pin<&mut Self>) {}
+// }
+
+// impl<T> SayHi for Vec<T> {
+//     fn say_hi(self: Pin<&Self>) {
+//         println!("Hi from Vec")
+//     }
+// }
+
+// impl<T> MutMeSomehow for Vec<T> {
+//     fn mut_me_somehow(self: Pin<&mut Self>) {
+//         let element = &self;
+//     }
+// }
+
+// impl SayHi for String {
+//     fn say_hi(self: Pin<&Self>) {
+//         println!("Hi from {:?}", self);
+//     }
+// }
+
+// impl MutMeSomehow for String {
+//     fn mut_me_somehow(self: Pin<&mut Self>) {
+//         *self = format!("{}{}", "prefix_", *self);
+//     }
+// }
+
+// impl SayHi for &[u8] {
+//     fn say_hi(self: Pin<&Self>) {
+//         println!("Hi from {:?}", self);
+//     }
+// }
+
+// impl MutMeSomehow for &[u8] {
+//     fn mut_me_somehow(self: Pin<&mut Self>) {
+//         self.reverse();
+//     }
+// }
+
+impl SayHi for i32 {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from {:?}", self);
+    }
+}
+
+impl MutMeSomehow for i32 {
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        *self = 5;
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    //    let b = Box::pin(5);
+    //    let b2 = Pin::new(5);
+    //    b.say_hi();
+
+    // let s = "".to_owned();
+    // println!("Box: {}", b);
+
+    let num: i32 = 5;
+    num.say_hi();
 }
