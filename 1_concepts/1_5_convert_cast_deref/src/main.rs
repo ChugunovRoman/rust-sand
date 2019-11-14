665ab95f36b0d4d6b041794d880e05ea4286e7a3
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-11-14 22:36:41 +0300

    Implement a EmailString type

--- 1_concepts/1_5_convert_cast_deref/src/main.rs
+++ 1_concepts/1_5_convert_cast_deref/src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{Error, ErrorKind};
+use std::ops::Deref;
+
+struct EmailString<'a> {
+    value: &'a str,
+}
+
+impl<'a> EmailString<'a> {
+    pub fn new(value: &'a str) -> Result<Self, Error> {
+        if !(value.contains("@") && value.contains(".")) {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "String is not a valid email string",
+            ));
+        }
+
+        Ok(EmailString { value })
+    }
+}
+
+impl<'a> Deref for EmailString<'a> {
+    type Target = &'a str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    let valid_email = EmailString::new("Vasya-Pupkin@ya.ru").unwrap();
+    let invalid_email = EmailString::new("Vasya-Pupkin@yaru");
+
+    println!("valid email: {}", *valid_email);
 }
