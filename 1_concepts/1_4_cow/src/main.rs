07c167bc77f3fc4d80b23c4c2c607f30a2e0a2c9
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-11-14 21:26:29 +0300

    Implement 1.4 task

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,27 @@
+use std::borrow::Cow;
+use std::env;
+
+struct Config<'a> {
+    path: Cow<'a, str>,
+}
+
 fn main() {
-    println!("Implement me!");
+    let mut config = Config {
+        path: Cow::Borrowed("/etc/app/app.conf"),
+    };
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        3 => (config.path = Cow::Borrowed(args.get(2).unwrap())),
+        _ => match env::var("APP_CONF") {
+            Ok(value) => {
+                if !value.is_empty() {
+                    config.path = Cow::Owned(value);
+                }
+            }
+            Err(err) => eprintln!("Cannot get the APP_CONF env, error: {}", err),
+        },
+    };
+
+    println!("App config path: {}", config.path);
 }
