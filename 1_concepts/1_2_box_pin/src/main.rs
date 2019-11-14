860f10e6345893f2cc843bbfa5309e5b20c92fe9
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-11-14 17:38:59 +0300

    Fixes for PR!4

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -1,100 +1,98 @@
-use std::fmt::{self, Debug, Display};
+use std::fmt::Debug;
+use std::ops::AddAssign;
 use std::pin::Pin;
 use std::rc::Rc;
 
 trait MutMeSomehow {
-    fn mut_me_somehow(self: &mut Self);
+    fn mut_me_somehow(self: Pin<&mut Self>);
 }
 
-trait SayHi {
-    fn say_hi(self: &Self) {
-        println!("Hi from");
+trait SayHi: Debug {
+    fn say_hi(self: Pin<&Self>) {
+        println!("Hi from {:?}", self);
     }
 }
 
-impl<T> SayHi for Box<T>
+impl<T> SayHi for Box<T> where T: Debug {}
+
+impl<T> MutMeSomehow for Box<T>
 where
-    T: Debug,
+    T: AddAssign + Copy,
 {
-    fn say_hi(self: &Self) {
-        println!("Hi from Box: {:?}", self)
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        let value = self.get_mut();
+        **value += **value;
     }
 }
 
-// impl<i32> MutMeSomehow for Box<i32> {
-//     fn mut_me_somehow(self: &mut Self) {
-//         let mut value = self.as_mut();
-//         value = value + 2;
-//     }
-// }
+impl<T> SayHi for Rc<T> where T: Debug {}
 
-// impl<T> SayHi for Rc<T> {
-//     fn say_hi(self: Pin<&Self>) {
-//         println!("Hi from Rc")
-//     }
-// }
-
-// impl<T> MutMeSomehow for Rc<T> {
-//     fn mut_me_somehow(self: Pin<&mut Self>) {}
-// }
-
-// impl<T> SayHi for Vec<T> {
-//     fn say_hi(self: Pin<&Self>) {
-//         println!("Hi from Vec")
-//     }
-// }
-
-// impl<T> MutMeSomehow for Vec<T> {
-//     fn mut_me_somehow(self: Pin<&mut Self>) {
-//         let element = &self;
-//     }
-// }
-
-// impl SayHi for String {
-//     fn say_hi(self: Pin<&Self>) {
-//         println!("Hi from {:?}", self);
-//     }
-// }
-
-// impl MutMeSomehow for String {
-//     fn mut_me_somehow(self: Pin<&mut Self>) {
-//         *self = format!("{}{}", "prefix_", *self);
-//     }
-// }
-
-impl SayHi for &[u8] {
-    fn say_hi(self: &Self) {
-        println!("Hi from {:?}", self);
+impl<T> MutMeSomehow for Rc<T>
+where
+    T: AddAssign + Copy,
+{
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        let value = self.get_mut();
+        let mut v = *value.clone();
+        v += v;
     }
 }
 
-// impl MutMeSomehow for &[u8] {
-//     fn mut_me_somehow(self: &mut Self) {
-//         *self.reverse();
-//     }
-// }
+impl<T> SayHi for Vec<T> where T: Debug {}
 
-impl SayHi for i32 {
-    fn say_hi(self: &Self) {
-        println!("Hi from {:?}", self);
+impl<T> MutMeSomehow for Vec<T>
+where
+    T: Unpin,
+{
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        let vector = self.get_mut();
+        vector.reverse();
     }
 }
+
+impl SayHi for String {}
+
+impl MutMeSomehow for String {
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        let value = self.get_mut();
+        *value = format!("{}{}", "prefix_", *value);
+    }
+}
+
+impl SayHi for &[u8] {}
+
+impl MutMeSomehow for &[u8] {
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        let value = self.get_mut();
+        *value = "other bytes".as_bytes();
+    }
+}
+
+impl SayHi for i32 {}
 
 impl MutMeSomehow for i32 {
-    fn mut_me_somehow(self: &mut Self) {
-        *self = *self << 1;
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        let value = self.get_mut();
+        *value = *value << 1;
     }
 }
 
 fn main() {
-    let b = Box::new(1);
-    b.say_hi();
+    Pin::new(&Box::new(5)).say_hi();
+    Pin::new(&mut Box::new(5)).mut_me_somehow();
 
-    // let s = "".to_owned();
-    // println!("Box: {}", b);
+    Pin::new(&Rc::new(666)).say_hi();
+    Pin::new(&mut Rc::new(666)).mut_me_somehow();
 
-    let mut num: i32 = 5;
-    num.say_hi();
-    num.mut_me_somehow();
-    println!("num: {}", num);
+    Pin::new(&vec![1, 2, 3]).say_hi();
+    Pin::new(&mut vec![1, 2, 3]).mut_me_somehow();
+
+    Pin::new(&String::from("str")).say_hi();
+    Pin::new(&mut String::from("str")).mut_me_somehow();
+
+    Pin::new(&"bytes".as_bytes()).say_hi();
+    Pin::new(&mut "bytes".as_bytes()).mut_me_somehow();
+
+    Pin::new(&i32::from(10)).say_hi();
+    Pin::new(&mut i32::from(10)).mut_me_somehow();
 }
